fca9df8f2dd6a0b63f4cf014a9cc6d763745730a
Pradyumna ds <pradyumnads7@gmail.com>
2023-05-30 02:58:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello world")
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def process_data():
+    a = request.args.get('a')
+    print("hello world")
